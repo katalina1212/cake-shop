148520aed88f24e773ce62d9ab00e7eb95e48a37
katalina1212 <53251633+katalina1212@users.noreply.github.com>
2020-08-12 22:12:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cakeshop/Cover.java
@@ -0,0 +1,30 @@
+package com.example.cakeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Cover extends AppCompatActivity {
+
+    private Button clickMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cover);
+        clickMenu=findViewById(R.id.click_menu);
+        clickMenu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openMenu();
+            }
+        });
+
+    }
+    private void openMenu(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/cakeshop/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.cakeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button FirebaseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseButton=findViewById(R.id.Firebase);
+        FirebaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+          public void onClick(View v) {
+                 // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+               // myRef.setValue("Hello, World!");
+                // Read from the database
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d("Firebase", "Value is: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w("Firebase", "Failed to read value.", error.toException());
+                    }
+                });
+            }
+        });
+
+    }
+
+}
